b5af60040d455e59a4d30ae15ed11565e0c4b056
UCHIHA MADARA <prabhuchaturvedi28@gmail.com>
2023-10-08 14:05:04 +0530

    ADDED DYNAMIC DATA AND API ROUTE

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,jsonify
 
 app = Flask(__name__)
 
@@ -44,6 +44,12 @@ def home():
     return render_template('home.html' , 
                            jobs=JOBS,
                            company_name='Jovian')
+
+
+@app.route("/api/jobs")
+def list():
+    return jsonify(JOBS)
+
 
 if __name__ == '__main__':
     app.run()
